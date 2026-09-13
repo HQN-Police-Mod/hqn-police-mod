a97320570ea6f2b664fdc5937a41fb544032ace8
alamritariq30-spec <alamritariq30@gmail.com>
2026-09-14 03:14:20 +0400

    Add C:/Users/oppc/Desktop/HQN POLICE MOD/hqn-police-mod/start-dev.bat

---
+++ C:/Users/oppc/Desktop/HQN POLICE MOD/hqn-police-mod/start-dev.bat
@@ -0,0 +1,11 @@
+@echo off
+title HQN POLICE MOD - Dev Server
+cd /d C:\Users\oppc\Desktop\HQNPOL~1\HQN-PO~1
+echo.
+echo  ==========================================
+echo   HQN POLICE MOD - Starting Dev Server
+echo  ==========================================
+echo.
+echo  http://localhost:3000
+echo.
+node node_modules\next\dist\bin\next dev
